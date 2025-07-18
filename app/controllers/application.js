99753fe23954cd20d822c269b3b74a779920e90a
Aad Versteden <madnificent@gmail.com>
2025-07-18 15:46:04 +0200

    Split off application into chat and task routes

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,36 +1,4 @@
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
-import { service } from '@ember/service';
 import Controller from '@ember/controller';
 
 export default class ApplicationController extends Controller {
-  @service store;
-  @service pushUpdates;
-
-  @tracked sender = ""
-  @tracked message;
-  @tracked messages;
-
-  constructor() {
-    super(...arguments);
-
-    this.pushUpdates.monitorCache({
-      path: "/messages?sort=-sent-at",
-      callback: async () =>
-        this.messages = await this.store.query('message', { sort: "-sent-at" })
-    });
-  }
-
-  resetMessage() {
-    this.message = this.store.createRecord('message');
-  }
-
-  @action
-  addMessage(event) {
-    event.preventDefault();
-    this.message.sentAt = new Date();
-    this.message.sender = this.sender;
-    this.message.save();
-    this.resetMessage();
-  }
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-  setupController(controller) {
-    super.setupController(...arguments);
-    controller.resetMessage();
-  }
 }
